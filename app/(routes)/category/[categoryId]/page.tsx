4116ff25a85aa6de88b7a4f8659ef0da30ec19f3
RabGh <rabiighais@gmail.com>
2023-09-03 21:43:32 +0300

    Add filter component for colors & sizes

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -3,7 +3,9 @@ import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
 import Container from "@/components/ui/container";
-import Billboard from "@/components/billboard";
+import Billboard from "@/components/ui/billboard";
+
+import Filter from "@/components/category/filter";
 
 export const revalidate = 0;
 
@@ -33,6 +35,14 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div>
       <Container>
         <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* Add Mobile Filters */}
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
