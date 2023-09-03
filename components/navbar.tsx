d0435cba8e547e190291eea101b55deca4a0146b
RabGh <rabiighais@gmail.com>
2023-09-03 15:40:43 +0300

    Add revalidation for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,8 @@ import Container from "@/components/ui/container";
 import getCategories from "@/actions/get-categories";
 import MainNav from "@/components/main-nav";
 
+export const revalidate = 0;
+
 const Navbar = async () => {
   const categories = await getCategories();
 
