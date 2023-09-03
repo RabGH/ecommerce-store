3b4202e70d3eb36884a6cda47ff94a5fc3b1e055
RabGh <rabiighais@gmail.com>
2023-09-03 12:47:32 +0300

    Add env file & fix footer description

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Ecommerce Store, Inc. All rights reserved.
+          2023 Ecommerce - rabiighais@gmai.com
         </p>
       </div>
     </footer>
