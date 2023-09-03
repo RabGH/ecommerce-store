e6ac5b06e4832ccbace5d6b4601299c784027ab0
RabGh <rabiighais@gmail.com>
2023-09-03 17:01:28 +0300

    Add images to product cards, home page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 import { Product } from "@/types";
 
 interface ProductCardProps {
@@ -9,7 +11,10 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   return (
     <div className="bg-white group cursor-pointer rounded-cl border p-3 space-y-4">
-      ProductCard
+      {/* Images and Actions */}
+      <div className="aspect-square rounded-cl bg-gray-100 relative">
+        <Image src={data?.images?.[0]?.url} alt="Image" fill></Image>
+      </div>
     </div>
   );
 };
