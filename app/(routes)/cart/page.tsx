698d9dea2ea2258b83701890d1117bd00483f672
RabGh <rabiighais@gmail.com>
2023-09-04 00:20:19 +0300

    Add cart functions & components & page

---
+++ app/(routes)/cart/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import CartItem from "@/components/cart/cart-item";
+import Container from "@/components/ui/container";
+import { useMount } from "@/hooks/mount-hook";
+import useCart from "@/hooks/use-cart";
+
+const CartPage = () => {
+  const isMounted = useMount();
+
+  const cart = useCart();
+
+  if (!isMounted) return null;
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items in cart.</p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CartPage;
