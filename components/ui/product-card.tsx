3c9b95bf505684eac49dc2e7adb4c5a541bdfa73
RabGh <rabiighais@gmail.com>
2023-09-03 17:05:57 +0300

    Add images & styles to product cards

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -10,10 +10,15 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   return (
-    <div className="bg-white group cursor-pointer rounded-cl border p-3 space-y-4">
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Images and Actions */}
-      <div className="aspect-square rounded-cl bg-gray-100 relative">
-        <Image src={data?.images?.[0]?.url} alt="Image" fill></Image>
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          src={data?.images?.[0]?.url}
+          alt="Image"
+          fill
+          className="aspect-sqaure object-cover rounded-md"
+        ></Image>
       </div>
     </div>
   );
