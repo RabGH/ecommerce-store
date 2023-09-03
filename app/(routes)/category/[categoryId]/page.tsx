c4f9a05a7d01a41b68b742dad71885a2345753df
RabGh <rabiighais@gmail.com>
2023-09-03 22:03:50 +0300

    Tablet / desktop filters

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -4,6 +4,8 @@ import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/ui/billboard";
+import ProductCard from "@/components/ui/product-card";
+import NoResults from "@/components/ui/no-results";
 
 import Filter from "@/components/category/filter";
 
@@ -36,10 +38,19 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
       <Container>
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
-          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+          <div className="lg:grid lg:grid-cols-2 lg:gap-x-8">
             {/* Add Mobile Filters */}
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
