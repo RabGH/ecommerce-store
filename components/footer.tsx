3d2aa3aeaae636c9824003e905abedfe403f2b2e
RabGh <rabiighais@gmail.com>
2023-09-03 15:39:23 +0300

    Add new footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          2023 Ecommerce - rabiighais@gmai.com
+          2023 Ecommerce -{" "}
+          <Link
+            href="https://rabiighais.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            rabiighais@gmail.com
+          </Link>
         </p>
       </div>
     </footer>
