b02c1cbffb5ca8640fa53b0c4708e520709d130f
RabGh <rabiighais@gmail.com>
2023-09-03 15:39:08 +0300

    Add error handling for get action files

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -2,10 +2,22 @@ import { Category } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL);
+console.log("API URL:", URL);
 
-  return res.json();
+const getCategories = async (): Promise<Category[]> => {
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      console.log("API Response:", res);
+      throw new Error(`API request failed with status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
+  }
 };
 
 export default getCategories;
