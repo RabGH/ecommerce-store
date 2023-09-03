d674efbd6436687b46feb4b0bf3dd34694905fe5
RabGh <rabiighais@gmail.com>
2023-09-03 23:01:23 +0300

    Add transition animations to dialog

--- components/category/mobile-filters.tsx
+++ components/category/mobile-filters.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { Plus, X } from "lucide-react";
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 
 import { Size, Color } from "@/types";
 import Button from "@/components/ui/button";
@@ -25,35 +25,40 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({ colors, sizes }) => {
         Filters
         <Plus size={20} />
       </Button>
-
-      <Dialog
-        open={open}
-        as="div"
-        className="relative z-40 lg:hidden"
-        onClose={onClose}
-      >
-        {/* Background */}
-        <div className="fixed inset-0 bg-black bg-opacity-25" />
-
-        {/* Dialog position */}
-        <div className="fixed inset-0 z-40 flex">
-          <Dialog.Panel
-            className="relative ml-auto flex h-full w-full max-w-xs flex-col 
-            overflow-y-auto bg-white py-4 pb-6 shadow-xl"
+      <Transition appear show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-40 lg:hidden" onClose={onClose}>
+          {/* Background */}
+          <div className="fixed inset-0 bg-black bg-opacity-25 transition-opacity duration-300 ease-in" />
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {/* Close Button */}
-            <div className="flex items-center justify-end px-4">
-              <IconButton icon={<X size={15} />} onClick={onClose} />
-            </div>
+            {/* Dialog position */}
+            <div className="fixed inset-0 z-40 flex">
+              <Dialog.Panel
+                className="relative ml-auto flex h-full w-full max-w-xs flex-col 
+            overflow-y-auto bg-white py-4 pb-6 shadow-xl"
+              >
+                {/* Close Button */}
+                <div className="flex items-center justify-end px-4">
+                  <IconButton icon={<X size={15} />} onClick={onClose} />
+                </div>
 
-            {/* Render the filter */}
-            <div className="p-4">
-              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
-              <Filter valueKey="colorId" name="Colors" data={colors} />
+                {/* Render the filter */}
+                <div className="p-4">
+                  <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+                  <Filter valueKey="colorId" name="Colors" data={colors} />
+                </div>
+              </Dialog.Panel>
             </div>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
     </>
   );
 };
